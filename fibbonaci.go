d9211e4a556af2c0d5b0b1849e442f953c034909
arjun <arjun.ep@awpl.co>
2017-01-27 18:18:28 +0530

    Fibbonaci_Reccursion

--- fibbonaci.go
+++ fibbonaci.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func fibu(num,a,b int) {
+func f(num,a,b int) {
 	if(num==0){
 		return
 	}
 	fmt.Println(a)
-	fibu(num-1,b,a+b)
+	f(num-1,b,a+b)
 }
 
 func main(){
-	fibu(8,0,1)
+	f(8,0,1)
 }
 	
